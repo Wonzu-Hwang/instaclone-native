bdc6d10888f5b77532ac1bf88cc21042b0b5ce69
Wonzu Hwang <dghkddnjswn@gmail.com>
2022-07-11 22:51:25 +0900

    until Search

--- App.js
+++ App.js
@@ -5,19 +5,35 @@ import { Asset } from "expo-asset";
 import LoggedOutNav from "./navigators/LoggedOutNav";
 import { NavigationContainer } from "@react-navigation/native";
 import { useState } from "react";
+import { ApolloProvider, useReactiveVar } from "@apollo/client";
+import client, { isLoggedInVar, tokenVar, cache } from "./apollo";
+import LoggedInNav from "./navigators/LoggedInNav";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { AsyncStorageWrapper, persistCache } from "apollo3-cache-persist";
+import Persistor from "apollo3-cache-persist/lib/Persistor";
 
 export default function App() {
   const [loading, setLoading] = useState(true);
   const onFinish = () => setLoading(false);
-  const preload = () => {
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const preloadAssets = () => {
     const fontsToLoad = [Ionicons.font];
     const fontPromises = fontsToLoad.map(font => Font.loadAsync(font));
-    const imagesToLoad = [
-      require("./assets/logo.png"),
-      "https://image.similarpng.com/very-thumbnail/2020/06/Instagram-name-logo-transparent-PNG.png",
-    ];
+    const imagesToLoad = [require("./assets/logo.png")];
     const imagePromises = imagesToLoad.map(image => Asset.loadAsync(image));
     return Promise.all([...fontPromises, ...imagePromises]);
+  };
+  const preload = async () => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      isLoggedInVar(true);
+      tokenVar(token);
+    }
+    await persistCache({
+      cache,
+      storage: new AsyncStorageWrapper(AsyncStorage),
+    });
+    return preloadAssets();
   };
 
   if (loading) {
@@ -30,8 +46,10 @@ export default function App() {
     );
   }
   return (
-    <NavigationContainer>
-      <LoggedOutNav />
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        {isLoggedIn ? <LoggedInNav /> : <LoggedOutNav />}
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
--- screens/CreateAccount.js
+++ screens/CreateAccount.js
@@ -1,10 +1,53 @@
 import React, { useRef } from "react";
-import { Platform, KeyboardAvoidingView } from "react-native";
 import AuthButton from "../components/auth/AuthButton";
 import AuthLayout from "../components/auth/AuthLayout";
 import { TextInput } from "../components/auth/AuthShared";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { gql, useMutation } from "@apollo/client";
 
-export default function CreateAccount() {
+const CREATE_ACCOUNT_MUTATION = gql`
+  mutation createAccount(
+    $firstName: String!
+    $lastName: String
+    $username: String!
+    $email: String!
+    $password: String!
+  ) {
+    createAccount(
+      firstName: $firstName
+      lastName: $lastName
+      username: $username
+      email: $email
+      password: $password
+    ) {
+      ok
+      error
+    }
+  }
+`;
+
+export default function CreateAccount({ navigation }) {
+  const { register, handleSubmit, setValue, getValues } = useForm();
+  const onCompleted = data => {
+    const {
+      createAccount: { ok },
+    } = data;
+    const { username, password } = getValues();
+    if (ok) {
+      navigation.navigate("LogIn", {
+        username,
+        password,
+      });
+    }
+  };
+  const [createAccountMutation, { loading }] = useMutation(
+    CREATE_ACCOUNT_MUTATION,
+    {
+      onCompleted,
+    }
+  );
+
   const lastNameRef = useRef();
   const usernameRef = useRef();
   const emailRef = useRef();
@@ -13,63 +56,74 @@ export default function CreateAccount() {
   const onNext = nextOne => {
     nextOne?.current?.focus();
   };
-  const onDone = () => {
-    alert("done!");
+  const onValid = data => {
+    if (!loading) {
+      createAccountMutation({
+        variables: {
+          ...data,
+        },
+      });
+    }
   };
+  useEffect(() => {
+    register("firstName", { required: true });
+    register("lastName", { required: true });
+    register("username", { required: true });
+    register("email", { required: true });
+    register("password", { required: true });
+  }, [register]);
 
   return (
     <AuthLayout>
-      <KeyboardAvoidingView
-        style={{
-          width: "100%",
-        }}
-        behavior="padding"
-        KeyboardVerticalOffset={Platform.OS === "ios" ? 50 : 0}
-      >
-        <TextInput
-          autoFocus
-          placeholder="First Name"
-          placeholderTextColor="gray"
-          returnKeyType="next"
-          onSubmitEditing={() => onNext(lastNameRef)}
-        />
-        <TextInput
-          ref={lastNameRef}
-          placeholder="Last Name"
-          placeholderTextColor="gray"
-          returnKeyType="next"
-          onSubmitEditing={() => onNext(usernameRef)}
-        />
-        <TextInput
-          ref={usernameRef}
-          placeholder="Username"
-          placeholderTextColor="gray"
-          returnKeyType="next"
-          onSubmitEditing={() => onNext(emailRef)}
-        />
-        <TextInput
-          ref={emailRef}
-          placeholder="Email"
-          placeholderTextColor="gray"
-          keyboardType="email-address"
-          returnKeyType="next"
-          onSubmitEditing={() => onNext(passwordRef)}
-        />
-        <TextInput
-          ref={passwordRef}
-          placeholder="Password"
-          placeholderTextColor="gray"
-          secureTextEntry
-          returnKeyType="done"
-          onSubmitEditing={onDone}
-          lastOne={true}
-        />
-        <AuthButton
-          text="Create Account"
-          disabled={true}
-          onPress={() => null}
-        />
-      </KeyboardAvoidingView>
+      <TextInput
+        placeholder="First Name"
+        returnKeyType="next"
+        placeholderTextColor={"rgba(255, 255, 255, 0.6)"}
+        onChangeText={text => setValue("firstName", text)}
+        onSubmitEditing={() => onNext(lastNameRef)}
+      />
+      <TextInput
+        ref={lastNameRef}
+        placeholder="Last Name"
+        returnKeyType="next"
+        placeholderTextColor={"rgba(255, 255, 255, 0.6)"}
+        onChangeText={text => setValue("lastName", text)}
+        onSubmitEditing={() => onNext(usernameRef)}
+      />
+      <TextInput
+        ref={usernameRef}
+        placeholder="Username"
+        returnKeyType="next"
+        autoCapitalize="none"
+        placeholderTextColor={"rgba(255, 255, 255, 0.6)"}
+        onChangeText={text => setValue("username", text)}
+        onSubmitEditing={() => onNext(emailRef)}
+      />
+      <TextInput
+        ref={emailRef}
+        placeholder="Email"
+        returnKeyType="next"
+        autoCapitalize="none"
+        keyboardType="email-address"
+        placeholderTextColor={"rgba(255, 255, 255, 0.6)"}
+        onChangeText={text => setValue("email", text)}
+        onSubmitEditing={() => onNext(passwordRef)}
+      />
+      <TextInput
+        ref={passwordRef}
+        placeholder="Password"
+        secureTextEntry
+        returnKeyType="done"
+        lastOne={true}
+        placeholderTextColor={"rgba(255, 255, 255, 0.6)"}
+        onChangeText={text => setValue("password", text)}
+        onSubmitEditing={handleSubmit(onValid)}
+      />
+      <AuthButton
+        text="Create Account"
+        onPress={handleSubmit(onValid)}
+        disabled={false}
+      />
     </AuthLayout>
   );
 }
